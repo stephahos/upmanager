5e3a96f3b5b865e7df8b3dda74e1fef1b86a5f6f
John Doe <atarod.shirin@gmail.com>
2022-11-24 22:09:42 +0100

    main page added

---
+++ src/components/AboutPage.jsx
@@ -0,0 +1,30 @@
+// import React from 'react'
+// import { Flex } from '@mantine/core';
+// import Header from "./Header";
+
+// function AboutPage() {
+//   return (
+    
+
+// function Demo() {
+//   return (
+//     <Flex
+//       mih={50}
+//       bg="rgba(0, 0, 0, .3)"
+//       gap="xl"
+//       justify="center"
+//       align="center"
+//       direction="row"
+//       wrap="wrap"
+//     >
+//       <img></img>
+//       <p></p>
+//       <img></img>
+//       <img></img>
+//     </Flex>
+//   );
+// }
+//   )
+// }
+
+// export default AboutPage
