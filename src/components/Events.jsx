a2a8622f2da248c35e1fabd4ab253b2cbee1a981
Stephanie AHossi <ahossi.stephanie@gmail.com>
2022-11-30 16:28:33 +0100

    changes

--- src/components/Events.jsx
+++ src/components/Events.jsx
@@ -1,1 +1,21 @@
- 
+import React from "react";
+import { useState, useEffect } from "react";
+import {
+  Divider,
+  Input,
+  Select,
+  createStyles,
+  Card,
+  Aside,
+} from "@mantine/core";
+import { useNavigate, Link, Navbar } from "react-router-dom";
+import { Image, Text, Badge, Button, Group } from "@mantine/core";
+import axios from "axios";
+import Sidebar from "../global/Sidebar";
+
+function Events() {
+  const [projects, setProjects] = useState([]);
+  return;
+}
+
+export default AllProjects;
